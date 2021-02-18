5596f41fc6713a977f6b9eea83d7af33b315377e
Christophe Adriaensens <christophe.adriaensens@gmail.com>
2021-02-18 16:26:49 +0100

    Minor code fix

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -5,15 +5,13 @@ const Weather = () => {
     const [temperature, findTemperature] = useState("");
     const [iconUrl, weatherIcon] = useState("");
 
-    const key = process.env.REACT_APP_WEATHER_API_KEY;
-
     useEffect(() => {
         showWeather();
     },[])
 
     // get inspirational quote
     const showWeather = () => {
-        Axios.get(`http://api.weatherbit.io/v2.0/current?city=ghent,belgium&key=${key}`).then((response) => {
+        Axios.get("http://api.weatherbit.io/v2.0/current?city=ghent,belgium&key=" + process.env.REACT_APP_WEATHER_API_KEY).then((response) => {
             console.log(response);
             let temperature = response.data.data[0].app_temp;
             let icon = response.data.data[0].weather.icon;
